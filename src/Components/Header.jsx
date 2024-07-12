4786f7178bb3a4a3183fd1f64be827405ac242bc
SiddharathMN <siddharathmn@gmail.com>
2024-07-12 17:55:42 +0400

    Add files via upload

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+function Header() {
+  return (
+    <div>
+    <header className="opacity-50" id="atthetop" >
+      <h1 className="mt-0">EduTech4Space</h1>
+    </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+function Hero() {
+  return (
+    //<div className="w-full flex justify-center items-center min-h-screen">
+  <div className="container max-w-2xl p-8 bg-crea rounded-lg shadow-lg text-center hover:opacity-90">
+    <h1 className="my-4 text-5xl font-bold">
+      Welcome to ET4S <br />Space Station 🚀🚀!!
+    </h1>
+    <p className="text-2xl mb-8">
+      ...lol we love making your life easier.
+    </p>
+    <div className="flex justify-center space-x-4">
+      <button className="hover:underline bg-gray-800 text-white font-bold rounded-full py-4 px-8">
+        View Projects
+      </button>
+      <button className="hover:underline bg-gray-800 text-white font-bold rounded-full py-4 px-8">
+        Plugins
+      </button>
+    </div>
+  </div>
+//</div>
+
+  );
+}
+
+export default Hero;
